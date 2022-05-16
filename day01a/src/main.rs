773e607329e62f18d645d7604e3ac3c3de90e638
Shiiyu <faithiikittii@gmail.com>
2022-05-16 17:59:48 -0400

    refactor: :recycle: refactor day 1 puzzle a

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,12 +1,10 @@
 fn main() {
   let input = include_str!("../input.txt").lines().map(|num| num.parse().unwrap()).collect::<Vec<u32>>();
 
-  for i in input.iter() {
-    for j in input.iter() {
-      if i == j {
-        continue;
-      } else if i + j == 2020 {
-        return println!("Expense Report: {}", i * j);
+  for (i, first) in input.iter().enumerate() {
+    for second in input.iter().skip(i + 1) {
+      if first + second == 2020 {
+        println!("Expense Report: {}", first * second);
       }
     }
   }
