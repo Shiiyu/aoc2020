91fdfca3808cbbc08afe418e32022ad93c7845cb
Shiiyu <faithiikittii@gmail.com>
2022-05-16 17:17:57 -0400

    feat: :sparkles: day 1 puzzle a

---
+++ day01a/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+  let input = include_str!("../input.txt").lines().map(|num| num.parse().unwrap()).collect::<Vec<u32>>();
+
+  for i in input.iter() {
+    for j in input.iter() {
+      if i + j == 2020 {
+        return println!("Expense Report: {}", i * j);
+      }
+    }
+  }
+}
