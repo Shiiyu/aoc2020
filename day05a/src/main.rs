08ec982e81a4a9fd76aa251d422bd7d5fad6b7d3
Shiiyu <faithiikittii@gmail.com>
2022-05-18 18:47:34 -0400

    feat: :sparkles: day 5 puzzle a

---
+++ day05a/src/main.rs
@@ -0,0 +1,10 @@
+pub fn main() {
+  println!(
+    "Highest Seat ID: {}",
+    include_bytes!("../input.txt")
+      .chunks(11)
+      .map(|b| b[..10].iter().fold(0, |id, b| (id << 1) | (!b & 4) as u16 >> 2))
+      .max()
+      .unwrap()
+  );
+}
