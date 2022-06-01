6fd98d9281a67fce8c0c6af1c1d50b60198244fa
Shiiyu <faithiikittii@gmail.com>
2022-06-01 14:16:37 -0400

    fix: :fire: remove unnecessary pub

--- day05b/src/main.rs
+++ day05b/src/main.rs
@@ -1,4 +1,4 @@
-pub fn main() {
+fn main() {
   let mut input: Vec<u16> = include_bytes!("../input.txt")
     .chunks(11)
     .map(|b| b[..10].iter().fold(0, |id, b| (id << 1) | (!b & 4) as u16 >> 2))
